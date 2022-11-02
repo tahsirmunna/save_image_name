8cc94b4458e84f67eb4c04d16907f39c16f273aa
Tahsir Ahmed Munna <tahsir.se@gmail.com>
2022-11-02 16:25:34 +0000

    Update save_image_name.py

--- save_image_name.py
+++ save_image_name.py
@@ -3,6 +3,7 @@ import os
 image_name=os.listdir("./train/") # get a list of image name from a folder
 
 #save the image name as .txt file. Here I name it list.txt
+#every line contain one image name
 with open("list.txt", "w") as f:
     for img in image_name:
         f.write(img+"\n") 
